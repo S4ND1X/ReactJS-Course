b343236e232bd8be7a30ba49898f24a20945816e
S4NDIX <sandix390@gmail.com>
2020-07-18 13:25:05 -0500

    Added routing structure

--- Project_Manager_MERN/project-manager/src/App.js
+++ Project_Manager_MERN/project-manager/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Login from './components/auth/login'
+import SignIn from './components/auth/sign-in'
+import Projects from './components/projects/projects'
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <h1>oa</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/sign-in" component={SignIn} />
+        <Route exact path="/projects" component={Projects} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
