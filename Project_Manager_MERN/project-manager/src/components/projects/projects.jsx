c06ac6112b5d5c4271948025f104730807c34c43
S4NDIX <sandix390@gmail.com>
2020-07-18 14:18:35 -0500

    Added Sidebar component

--- Project_Manager_MERN/project-manager/src/components/projects/projects.jsx
+++ Project_Manager_MERN/project-manager/src/components/projects/projects.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 
+// Component imports
+
+import Sidebar from "../layout/Sidebar";
+
 export default function Projects() {
   return (
-    <div>
+    <div className="contenedor-app">
+      <Sidebar />
+      <div className="seccion-principal">
+        <main>
+          <div className="contenedor-tareas"></div>
+        </main>
+      </div>
       <h1>Projects</h1>
     </div>
   );
